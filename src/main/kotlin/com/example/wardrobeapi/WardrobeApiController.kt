d037509e1f1f05036c20874d60ff6bbfe9dc5a22
tk_hirom <takahiroinkyoto@gmail.com>
2024-04-15 15:33:36 +0900

    不要なアノテーションの削除

--- src/main/kotlin/com/example/wardrobeapi/WardrobeApiController.kt
+++ src/main/kotlin/com/example/wardrobeapi/WardrobeApiController.kt
@@ -1,11 +1,10 @@
 package com.example.wardrobeapi
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class WardrobeApiController(@Autowired private val appProperties: AppProperties) {
+class WardrobeApiController( private val appProperties: AppProperties) {
 
     @GetMapping("/my_best_cloth")
     fun getALlClothes(): Cloth {
